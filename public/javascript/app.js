ab6fa8f20d8e969eb3a8d5f4cf8979a4f436c9d6
Trang Doan <doantranghp2000@gmail.com>
2021-03-07 18:22:51 +0900

    Remove unnecessary space

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -5,8 +5,6 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
